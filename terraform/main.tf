dc637cb2da113bf8e47a37b7d8a1066ae2cfaf99
Raj Pratap Singh Manda <raj.21bcon514@jecrcu.edu.in>
2025-04-16 09:19:09 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,8 +28,19 @@ resource "azurerm_kubernetes_cluster" "aks" {
     vm_size    = "Standard_DS2_v2"
     
   }
-
   identity {
     type = "SystemAssigned"
   }
+  acr_attach {
+    acr_name = "mandacontainer2342423"
+  }
+
 }
+
+resource "azurerm_role_assignment" "acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+}
+
+
